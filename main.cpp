88950041176786353ea333823dc5674fe6ad8c6f
susant.pant <susant.pant@zone42-wb.cs.ucalgary.ca>
2017-02-26 15:22:11 -0700

    cleaned up code and commented a bunch of it

--- main.cpp
+++ main.cpp
@@ -77,7 +77,7 @@ void render () {
 	//gluPerspective (fov, aspect ratio, near plane, far plane)
 	//glFrustum
 
-	//Draws the points as little circles
+	//Draws the control points as little white circles
 	for (int i = 0; i < controls.size() - 1; i++){
 		glBegin (GL_TRIANGLE_STRIP); //GL_LINE_STRIP, GL_POINTS, GL_QUADS, etc...
 			glColor3f(1.f, 1.f, 1.f);
@@ -89,6 +89,20 @@ void render () {
 		glEnd ();
 	}
 
+	// Draws the knots as little red circles
+	for (int i = 0; i < knots.size(); i++){
+	glBegin (GL_TRIANGLE_STRIP); //GL_LINE_STRIP, GL_POINTS, GL_QUADS, etc...
+		glColor3f(1.f, 0.f, 0.f);
+		for (float t = 0.f; t < 2*PI; t += 0.01f){
+			vec2 circle = vec2(cRadius*cos(t), cRadius*sin(t));
+			vec2 knotPos = findPosAt(knots[i]);
+			glVertex2f(knotPos.x + circle.x, knotPos.y + circle.y);
+			glVertex2f(knotPos.x, knotPos.y);
+		}
+	glEnd();
+	}
+
+	// draws the whole curve
 	glBegin (GL_LINE_STRIP);
 	glColor3f(1.f, 1.f, 1.f);
 	for(float u = knots[order - 1]; u <= knots[controls.size() + 1]; u += (0.001)){
@@ -97,6 +111,7 @@ void render () {
 	}
 	glEnd();
 
+	// draws the current position of the user's 'u' position as a red X.
 	glBegin (GL_LINES);
 	vec2 uPos = findPosAt(uParam);
 	glColor3f(1.f, 0.f, 0.f);
@@ -105,18 +120,6 @@ void render () {
 	glVertex2f(uPos.x + 2*cRadius, uPos.y + 2*cRadius);
 	glVertex2f(uPos.x - 2*cRadius, uPos.y - 2*cRadius);
 	glEnd();
-
-	for (int i = 0; i < knots.size(); i++){
-		glBegin (GL_TRIANGLE_STRIP); //GL_LINE_STRIP, GL_POINTS, GL_QUADS, etc...
-			glColor3f(1.f, 0.f, 0.f);
-			for (float t = 0.f; t < 2*PI; t += 0.01f){
-				vec2 circle = vec2(cRadius*cos(t), cRadius*sin(t));
-				vec2 knotPos = findPosAt(knots[i]);
-				glVertex2f(knotPos.x + circle.x, knotPos.y + circle.y);
-				glVertex2f(knotPos.x, knotPos.y);
-			}
-		glEnd();
-	}
 }
 
 void buildKnots(){
@@ -150,8 +153,10 @@ void keyboard (GLFWwindow *sender, int key, int scancode, int action, int mods) 
 	if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
 		glfwSetWindowShouldClose(window, GL_TRUE);
 	if (key == GLFW_KEY_UP && action == GLFW_PRESS){
+		//check that the new order is not out of bounds
 		if (order + 1 <= controls.size() - 1){
 			order++;
+			//change the value of the knots to reflect new 'k' value
 			knots.insert(knots.begin(), 0.f);
 			knots.push_back(1.f);
 			buildKnots();
@@ -161,8 +166,10 @@ void keyboard (GLFWwindow *sender, int key, int scancode, int action, int mods) 
 		}
 	}
 	else if (key == GLFW_KEY_DOWN && action == GLFW_PRESS){
+		//check that the new order is not out of bounds
 		if (order > 2){
 			order--;
+			//change the value of the knots to reflect new 'k' value
 			knots.erase(knots.begin());
 			knots.pop_back();
 			buildKnots();
@@ -172,6 +179,7 @@ void keyboard (GLFWwindow *sender, int key, int scancode, int action, int mods) 
 		}
 	}
 	if (key == GLFW_KEY_RIGHT && action == GLFW_PRESS){
+		//check that the u parameter is not out of bounds
 		if (uParam < knots[controls.size()]){
 			uParam += 0.05;
 			if (uParam > 1.f){
@@ -181,9 +189,11 @@ void keyboard (GLFWwindow *sender, int key, int scancode, int action, int mods) 
 		else {
 			cout << "Highest u value reached." << endl;
 		}
+		//in case you need to print out uParam, press Enter
 		//cout << uParam << endl;
 	}
 	else if (key == GLFW_KEY_LEFT && action == GLFW_PRESS){
+		//check that the u parameter is not out of bounds
 		if (uParam > knots[order - 1]){
 			uParam -= 0.05;
 			if (uParam < 0.f){
@@ -193,8 +203,10 @@ void keyboard (GLFWwindow *sender, int key, int scancode, int action, int mods) 
 		else {
 			cout << "Lowest u value reached." << endl;
 		}
+		//in case you need to print out uParam, press Enter
 		//cout << uParam << endl;
 	}
+	//pressing Enter prints the current statistics of the curve
 	if (key == GLFW_KEY_ENTER && action == GLFW_PRESS){
 			cout << "Order: " << order << endl;
 			cout << "Number of Points: " << controls.size() - 1 << endl;
@@ -208,26 +220,34 @@ void mouseClick (GLFWwindow *sender, int button, int action, int mods) {
 	selected = -1;
 	canMove = false;
 	if (action == GLFW_PRESS){
+		// find the selected control point
 		for (int i = 0; i < controls.size() - 1; i++){
 			if ((abs(controls[i].x - mouseX) <= cRadius) && (abs(controls[i].y - mouseY) <= cRadius)){
 				selected = i;
 			}
 		}
 		if (button == GLFW_MOUSE_BUTTON_LEFT) {
+			// insert a point if no control point is identified at mousePos
 			if (selected == -1){
 				controls.insert(controls.begin() + controls.size() - 1, vec2(mouseX, mouseY));
 				cout << "New control point: " << controls.size() - 1 << endl;
 			}
-			else{
+			// select a point if a control point is identified at mousePos
+			else {
 					cout << "Selected control point: " << selected + 1 << endl;
 			}
+			// either way, the control point's position can now be modified
 			canMove = true;
 		}
 		if (button == GLFW_MOUSE_BUTTON_RIGHT){
 			int ptsCheck = controls.size() - 3;
 			int ordCheck = order - 2;
+			//bounds checking to see if deleting a point is possible
+			//ptsCheck and ordCheck have to be integers.
+			// 	You could typecast them inside the compare statement but that looks ugly
 			if ((ptsCheck > ordCheck) && (ptsCheck > 0)){
 				if (selected != -1){
+					// delete the selected point
 					controls.erase(controls.begin() + selected);
 					cout << "Deleted control point: " << selected << endl;
 				}
@@ -237,12 +257,15 @@ void mouseClick (GLFWwindow *sender, int button, int action, int mods) {
 			}
 		}
 		if (button == GLFW_MOUSE_BUTTON_MIDDLE) {
+			// insert a control point in the curve in the index of the currently selected control point
 			if (selected != -1){
 				controls.insert(controls.begin() + selected, vec2(mouseX, mouseY));
 				cout << "New control point: " << selected << endl;
 			}
+			//the control point's position can now be modified
 			canMove = true;
 		}
+		//change the knot values to reflect changes you made (even if no changes were made, i.e. if a point was moved)
 		buildKnots();
 	}
 }
@@ -251,6 +274,7 @@ void mousePos (GLFWwindow *sender, double x, double y) {
 	mouseX = (2 * x / w) - 1;
 	mouseY = -((2 * y / h) - 1);
 	if (selected != -1 && canMove){
+		//moves the currently selected point
 		controls[selected].x = mouseX;
 		controls[selected].y = mouseY;
 	}
